14f756287a2c79a8c99177f2014f3d8ca6e9d409
camilorubio55 <camilorubiocontreras@gmail.com>
2024-07-22 12:36:56 -0500

    The Matcher file was added for use on error tests

---
+++ app/src/main/java/com/example/weather/extensions/Matcher.kt
@@ -0,0 +1,13 @@
+package com.example.weather.extensions
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.MatcherAssert.assertThat
+
+inline fun <reified T> classType(): Class<T> = T::class.java
+
+inline fun <reified T> assertThatIsInstanceOf(actual: Any?) = assertThat(
+        actual, instanceOf(classType<T>())
+)
+
+fun assertThatEquals(actual: Any?, expected: Any?) = assertThat(actual, equalTo(expected))
